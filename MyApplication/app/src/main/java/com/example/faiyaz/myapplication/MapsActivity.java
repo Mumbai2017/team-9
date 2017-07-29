1a4104f0ead0f8e90203466dae10d05fbfa1cba1
Sanya <sanya.shaikh82@gmail.com>
2017-07-30 03:40:56 +0530

    layout

--- MyApplication/app/src/main/java/com/example/faiyaz/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/faiyaz/myapplication/MapsActivity.java
@@ -5,14 +5,12 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
 import java.io.IOException;
 import java.util.List;
 
