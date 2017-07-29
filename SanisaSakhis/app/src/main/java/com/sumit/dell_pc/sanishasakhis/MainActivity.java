ff105f25a1572a45e528fc55aeccb6f2003a6187
Sumit Rasikbhai Busa <sumitbusa@gmail.com>
2017-07-29 16:41:52 +0530

    updated

--- SanisaSakhis/app/src/main/java/com/sumit/dell_pc/sanishasakhis/MainActivity.java
+++ SanisaSakhis/app/src/main/java/com/sumit/dell_pc/sanishasakhis/MainActivity.java
@@ -3,6 +3,8 @@ package com.sumit.dell_pc.sanishasakhis;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -20,17 +22,25 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.sumit.dell_pc.sanishasakhis.language.HomeFragment;
+import com.sumit.dell_pc.sanishasakhis.language.TopFreeFragment;
+import com.sumit.dell_pc.sanishasakhis.language.TopPaidFragment;
+import com.sumit.dell_pc.sanishasakhis.language.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private String TAG = "MainActivity";
 
     private Bundle bundle;
-    private Toolbar toolbar;
     private FirebaseAuth mAuth;
     private MaterialSearchView searchView;
 
     private GoogleApiClient mGoogleApiClient;
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +74,25 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         //Search Menu Item
         searchView = (MaterialSearchView) findViewById(R.id.search_view);
         searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new HomeFragment(), "Inventory");
+        viewPagerAdapter.addFragments(new TopFreeFragment(), "Stock request");
+        viewPagerAdapter.addFragments(new TopPaidFragment(), "Customer Order");
+        viewPagerAdapter.addFragments(new TopPaidFragment(), "Work Colleague");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setSelectedTabIndicatorColor(Color.WHITE);
+        tabLayout.setTabTextColors(Color.WHITE, Color.WHITE);
     }
 
     @Override
