57ccdaf95f557d78017699fb660f536e7f6acf94
khyati <khyati.s.96@gmail.com>
2017-07-29 17:05:49 +0530

    model files

---
+++ SanisaSakhis/app/src/main/java/com/sumit/dell_pc/sanisasakhis/models/cus.java
@@ -0,0 +1,27 @@
+package com.sumit.dell_pc.sanisasakhis.models;
+
+
+public class cus {
+    public String username;
+    public int userId;
+    public String email;
+    public String fname;
+    public String lname;
+    public int mobno;
+    public int orderno;
+    public String address; 
+    public cus()
+    {}
+    public cus(String username,int userId,String email,String fname,String lname,int mobno, int orderno,String address)
+    {
+		this.username = username;
+		this.userId=userId;
+        this.email=email;
+        this.fname=fname;
+        this.lname=lname;
+        this.mobno=mobno;
+        this.orderno=orderno;
+        this.address=address;
+    }
+
+}
